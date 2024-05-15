b06ad26cd9be49ebabb3c5b8ef7ac2e96a656cfe
Partner0487 <partner0487@gmail.com>
2024-05-15 23:31:36 +0800

    Initial commit

---
+++ app/src/main/java/ntou/android2024/ntou_credit_calculation/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package ntou.android2024.ntou_credit_calculation.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
