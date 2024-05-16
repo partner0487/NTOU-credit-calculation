96018a7d36de9f82aefa0c021766c52fbe469f29
Partner0487 <partner0487@gmail.com>
2024-05-16 20:29:27 +0800

    add name、id、chinese

--- app/src/main/java/ntou/android2024/ntou_credit_calculation/ui/home/HomeFragment.kt
+++ app/src/main/java/ntou/android2024/ntou_credit_calculation/ui/home/HomeFragment.kt
@@ -6,7 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.launch
 import ntou.android2024.ntou_credit_calculation.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -22,15 +27,30 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        val viewModel: HomeViewModel by viewModels()
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
+
+        val textView1: TextView = binding.textName
+        textView1.text = "名字"
+
         homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+            //textView1.text = it
+        }
+        val numberView1: TextView = binding.numberId
+        numberView1.text = "學號"
+
+        viewModel.rollDice()
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect {
+                    //numberView1.text = it.firstDieValue.toString()
+                }
+            }
         }
         return root
     }
--- app/src/main/java/ntou/android2024/ntou_credit_calculation/ui/home/HomeViewModel.kt
+++ app/src/main/java/ntou/android2024/ntou_credit_calculation/ui/home/HomeViewModel.kt
@@ -3,6 +3,17 @@ package ntou.android2024.ntou_credit_calculation.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlin.random.Random
+
+data class DiceUiState(
+    val firstDieValue: Int? = null,
+    val secondDieValue: Int? = null,
+    val numberOfRolls: Int = 0,
+)
 
 class HomeViewModel : ViewModel() {
 
@@ -10,4 +21,20 @@ class HomeViewModel : ViewModel() {
         value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
+
+    // Expose screen UI state
+    private val _uiState = MutableStateFlow(DiceUiState())
+    val uiState: StateFlow<DiceUiState> = _uiState.asStateFlow()
+
+    // Handle business logic
+    fun rollDice() {
+        _uiState.update { currentState ->
+            currentState.copy(
+                firstDieValue = Random.nextInt(from = 1, until = 7),
+                secondDieValue = Random.nextInt(from = 1, until = 7),
+                numberOfRolls = currentState.numberOfRolls + 1,
+            )
+        }
+    }
+
 }
